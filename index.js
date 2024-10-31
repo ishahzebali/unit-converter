0bbe692a8173b1930bc963d10841ff2245e05d96
Shahzeb Ali <shahzebali@Shahzebs-MacBook.local>
2024-10-31 20:48:04 +0400

    added database for fun

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js"
+
+const firebaseConfig = {
+    databaseURL : "https://practice-2cfeb-default-rtdb.firebaseio.com"
+  }
+
+  const app = initializeApp(firebaseConfig)
+  const database = getDatabase(app)
+  const referenceInDB = ref(database, "unit")
+
+
 const mainInput = document.getElementById("main-input")
 const lengthP = document.getElementById("length-p")
 const volumeP = document.getElementById("volume-p")
@@ -8,11 +20,22 @@ let meter = 3.281
 let liter = 0.264
 let kilogram = 2.204
 
-convertBtn.addEventListener("click", function(inputValue) {
+onValue(referenceInDB, function(snapshot) {
+    const doesSnapshotExists = snapshot.exists()
 
-    inputValue = parseFloat(mainInput.value);
+    if (doesSnapshotExists) {
+        const snapshotValue = snapshot.val()
+        const units = Object.values(snapshotValue)
+        console.log("Units from DB:", units)
+    }
+})
 
+convertBtn.addEventListener("click", function() {
+
+    const inputValue = parseFloat(mainInput.value);
+    
     if (!isNaN(inputValue)) {
+        push(referenceInDB, inputValue)
 
         lengthP.innerHTML = `${inputValue} meters = ${(inputValue * meter).toFixed(3)} feet |
         ${inputValue} feet = ${(inputValue / meter).toFixed(3)} meter`
@@ -31,6 +54,7 @@ convertBtn.addEventListener("click", function(inputValue) {
     
 })
 
-clearBtn.addEventListener("dblclick", function(){
+clearBtn.addEventListener("click", function(){
     mainInput.value = ""
+    remove(referenceInDB)
 })
